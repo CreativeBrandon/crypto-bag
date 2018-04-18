2f3118c60b88ccd6991ef5502527ddcdbf0392e3
creativebrandon <admin@cre8tivebreed.com>
2018-04-18 16:55:09 -0400

    new util func

--- src/components/NumericTextField.vue
+++ src/components/NumericTextField.vue
@@ -10,7 +10,7 @@
 
 <script lang="ts">
 import { Component, Emit, Prop, Vue } from 'vue-property-decorator'
-import { isNumeric } from '../utils/index'
+import { isNumeric } from '@/utils'
 
 @Component
 export default class NumericTextField extends Vue {
@@ -18,14 +18,14 @@ export default class NumericTextField extends Vue {
   @Prop({ default: 'Amount of Coins' }) placeholder?: string
   @Prop({ default: 0 }) value?: number
 
-  isAllowed(key: string): boolean{
+  isAllowed(key: string): boolean {
     const allowedKeys = ['.', 'Backspace', 'Tab'] as string[]
     if (allowedKeys.indexOf(key) > -1 || isNumeric(key)) return true
     return false
   }
 
-  onKeypress(e: KeyboardEvent) {    
-    if (!this.isAllowed(e.key)) e.preventDefault() 
+  onKeypress(e: KeyboardEvent) {
+    if (!this.isAllowed(e.key)) e.preventDefault()
   }
 }
 </script>
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,3 +6,9 @@ export const objFromArray = (array: any[]): types.ByIds => {
         return accumulator
     }, {})
 }
+
+export const isNumeric = (value: string, regex: RegExp = /^[0-9]*$/): boolean => {
+    const regex2 = /^[0-9\.]*$/
+    if (regex.test(value)) return true
+    return false
+}
